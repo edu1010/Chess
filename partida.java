6b9f430887f83d7908dc233cca6df9e42a703c85
eduard <cf17eduard.corral@iesjoandaustria.org>
2018-05-09 19:54:11 +0200

    REI

---
+++ partida.java
@@ -0,0 +1,8 @@
+public class partida {
+    public static void main(String[] args) {
+        public jugador1=new Jugador(setEmail(String email,setColorPeça(String color)));
+        public jugador2=new Jugador(setEmail(String email,setColorPeça(String color)));
+
+    }
+
+}
