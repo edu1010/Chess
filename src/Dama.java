21e1ec5bab8a811a2faeab610f8cebe1b7852736
xavitolavi <cf17xavier.florit@iesjoandaustria.org>
2018-05-09 20:01:28 +0200

    9/5 damatauler

---
+++ src/Dama.java
@@ -0,0 +1,10 @@
+
+public class Dama{
+	public String nom="Dama";
+	public String color="Blanc";
+	Public int posicio
+	public Reina() {
+		
+	}
+	
+}
---
+++ src/Tauler.java
@@ -0,0 +1,27 @@
+
+public class Tauler {
+	private int x=8;
+	private int y=8;
+	private int caselles[][] = new int[x][y];
+	
+	public Tauler() {
+		for (int i=0;i<x;i++) {
+			for(int j=0;j<y;j++) {
+				
+			}
+		}
+	}
+	
+	private String detectarGuanyador() {
+		return "blanc";
+	}
+	
+	private boolean acabarPartida() {
+		return true;
+	}
+	
+	public boolean torn() {
+		return true;
+	}
+	
+}
